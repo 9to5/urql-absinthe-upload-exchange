00c85aba588314866c76eaa444943123ab451d4f
Tim de Booij <tim.debooij@9to5.software>
2024-07-01 22:31:05 +0200

    fix: revert release.config.js to module.exports

--- release.config.js
+++ release.config.js
@@ -1,16 +1,18 @@
-export const branches = ["master"];
-export const plugins = [
-  "@semantic-release/commit-analyzer",
-  "@semantic-release/release-notes-generator",
-  "@semantic-release/changelog",
-  "@semantic-release/npm",
-  "@semantic-release/github",
-  [
-    "@semantic-release/git",
-    {
-      assets: ["package.json", "CHANGELOG.md"],
-      message:
-        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    },
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
-];
+};
