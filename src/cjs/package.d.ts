1580af5777cdd31cd01d7aa5865eccbeb93d9ddc
Tim de Booij <t.debooij@gmail.com>
2025-04-28 22:59:54 +0200

    fix: remove old cjs files

--- src/cjs/package.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.js' {
-  import * as mod from '../index';
-  export = mod;
-}
-
-declare module '*.cjs' {
-  import * as mod from '../index';
-  export = mod;
-}
--- src/esm/package.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.mjs' {
-  export * from '../index';
-  export { default } from '../index';
-}
